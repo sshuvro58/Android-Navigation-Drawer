ef46c0a4dace9667bb7ed9c2ca6514f05c2af70a
sshuvro <sshuvro58@gmail.com>
2015-09-05 00:51:43 +0600

    first commit

---
+++ app/src/main/java/com/paranoidlab/navigatiodrawerfragmentsample/MainActivity.java
@@ -0,0 +1,67 @@
+package com.paranoidlab.navigatiodrawerfragmentsample;
+
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    private DrawerLayout mdrawerLayout;
+    private ListView listView;
+    private String[] planets;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mdrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        listView = (ListView) findViewById(R.id.drawerlist);
+        planets = getResources().getStringArray(R.array.planets);
+        listView.setAdapter(new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,planets));
+        listView.setOnItemClickListener(this);
+     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(this,planets[position],Toast.LENGTH_LONG).show();
+        selectItem(position);
+    }
+
+    private void selectItem(int position) {
+        listView.setItemChecked(position, true);
+        setTtitle(planets[position]);
+    }
+
+    private void setTtitle(String planet) {
+        getSupportActionBar().setTitle(planet);
+    }
+
+}
