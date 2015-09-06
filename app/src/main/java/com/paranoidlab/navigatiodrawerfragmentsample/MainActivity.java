c02a2130593cfe1b9da174faaf142eb017ad214b
sshuvro <sshuvro58@gmail.com>
2015-09-06 23:52:17 +0600

    second commit

--- app/src/main/java/com/paranoidlab/navigatiodrawerfragmentsample/MainActivity.java
+++ app/src/main/java/com/paranoidlab/navigatiodrawerfragmentsample/MainActivity.java
@@ -1,6 +1,11 @@
 package com.paranoidlab.navigatiodrawerfragmentsample;
 
+import android.annotation.SuppressLint;
+import android.content.res.Configuration;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -16,6 +21,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     private DrawerLayout mdrawerLayout;
     private ListView listView;
     private String[] planets;
+    private ActionBarDrawerToggle drawerListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,9 +29,36 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         mdrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
         listView = (ListView) findViewById(R.id.drawerlist);
         planets = getResources().getStringArray(R.array.planets);
-        listView.setAdapter(new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,planets));
+        listView.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, planets));
         listView.setOnItemClickListener(this);
+        mdrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+      //  drawerListener = new ActionBarDrawerToggle(this,mdrawerLayout,R.drawable.ic_stat_mobmenuicon,"Open Drawer","Close Drawer");
+        drawerListener = new ActionBarDrawerToggle(this,mdrawerLayout,R.string.drawer_open,R.string.drawer_close){
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+        };
+        mdrawerLayout.setDrawerListener(drawerListener);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
      }
+
+
+
+
+    @Override
+    public void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerListener.syncState();
+    }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -35,17 +68,25 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     }
 
     @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerListener.onConfigurationChanged(newConfig);
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+        if(drawerListener.onOptionsItemSelected(item)){
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
